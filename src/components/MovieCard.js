8271891c7f8aab1128bdb4f4e2856ff8b0415d81
core.pager=cat - <mikepool599@gmail.com>
2021-11-15 08:53:04 +0100

    added addToFavourites

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,48 +1,36 @@
-import React,{useState, useEffect} from 'react';
-import "../assests/style/MovieCard.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MovieList from '../components/MovieList';
-import MovieListHeading from './Header';
-import Filter from './Filter'
-import AddFavourites from './AddToFavourites';
-import RemoveFavourites from './RemoveFromFavourites';
-//import Movies from '../assests/data/Movie'
-
+import React, { useState } from "react";
+import "../assests/style/MovieCard.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieList from "../components/MovieList";
+import MovieListHeading from "./Header";
+import Filter from "./Filter";
+import AddFavourites from "./AddToFavourites";
+import RemoveFavourites from "./RemoveFromFavourites";
 
 const MovieCard = () => {
-    const [movies, setMovies] = useState([]);
-    const [favourites, setFavourites] = useState([]);
-    const [searchValue, setSearchValue] = useState('')
-    const [imdbList, setImdbList] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [imdbList, setImdbList] = useState([]);
 
-    const getMovieRequest = async (searchValue) => {
-        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b0967f48&page=1`;
-        //const url =   `http://www.omdbapi.com/?t=${searchValue}&apikey=b0967f48`;
+  const getMovieRequest = async (searchValue) => {
+    setSearchValue(searchValue);
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b0967f48&page=1`;
 
-        
+    const response = await fetch(url);
+    const responseJson = await response.json();
 
-        const response = await fetch(url); 
-        const responseJson = await response.json();
+    const movies = responseJson.Response === "True" ? responseJson.Search : [];
+    const imdbIds =
+      movies.length > 0 ? movies.map((movie) => movie.imdbID) : [];
 
-        
+    setImdbList(imdbIds);
+  };
 
-        if (responseJson.Search){
-            const searchList = responseJson.Search
-           // console.log(searchList, 'me')
-            const imdbIds = searchList.map(({imdbID}) => imdbID)
-            //console.log(imdbIds, 'me')
-            setImdbList(imdbIds)
-            //console.log(imdbIds)
-            //console.log(responseJson.Search)
+  // useEffect(() => {
+  //     getMovieRequest(searchValue);
+  // }, [searchValue]);
 
-            setMovies(responseJson.Search);
-        }
-    }; 
-    useEffect(() => {
-        getMovieRequest(searchValue);
-    }, [searchValue]);
-
-   /* useEffect(() => {
+  /* useEffect(() => {
         const movieFavourites = JSON.parse(
             localStorage.getItem('movieapp-favourites')
         );
@@ -55,51 +43,59 @@ const MovieCard = () => {
     };
     */
 
+  const addFavouriteMovie = (imdbID) => {
+    console.log(imdbID);
+    const newFavouriteList = [...favourites, imdbID];
+    setFavourites(newFavouriteList);
+    //saveToLocalStorage(newFavouriteList)
+  };
 
-    const addFavouriteMovie = (movie) => {
-        const newFavouriteList = [...favourites, movie];
-        setFavourites (newFavouriteList)
-        //saveToLocalStorage(newFavouriteList)
-        
-    };
-    
-    const removeFavouritesMovie = (movie) => {
-        const newFavouriteList = favourites.filter(
-            (favourite) => favourite.imdbID !== movie.imdbID
-        );
-
-        setFavourites(newFavouriteList);
-    };
-
-    return(
-        <div className= 'container-fluid movie-app'>
-            <div className = 'row d-flex align-items-center mt-4 mb-4 movie-heading'>
-                <MovieListHeading heading = 'Mikeflix'/>
-                <Filter  searchValue={searchValue} setSearchValue={setSearchValue} />
-            </div>
-
-            <div className='row'>
-                <MovieList
-                    //movies= {movies} 
-                    movieIdList = {imdbList} 
-                    handleFavouritesClick= {addFavouriteMovie} 
-                    favouriteComponent= {AddFavourites}
-                />
-            </div>
-
-            {/* <div className = 'row d-flex align-items-center mt-4 mb-4'>
-                <MovieListHeading heading = 'Favourite Movies'/>
-            </div>
-            <div className='row'>
-                <MovieList
-                    movies= {favourites} 
-                    handleFavouritesClick= {removeFavouritesMovie} 
-                    favouriteComponent= {RemoveFavourites}
-                />
-            </div> */}
-        </div>
+  const removeFavouritesMovie = (imdbID) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite !== imdbID
     );
-};
 
+    setFavourites(newFavouriteList);
+  };
+
+  console.log(favourites);
+
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4 movie-heading">
+        <MovieListHeading heading="Mikeflix" />
+        <Filter searchValue={searchValue} setSearchValue={getMovieRequest} />
+      </div>
+
+      <div className="row">
+        {imdbList.length > 0 && (
+          <MovieList
+            searchValue={searchValue}
+            movieIdList={imdbList}
+            favourites={favourites}
+            handleFavouritesClick={addFavouriteMovie}
+            favouriteComponent={AddFavourites}
+          />
+        )}
+        {searchValue && imdbList.length === 0 && (
+          <div className="alert alert-danger" role="alert">
+            No movies found for {searchValue}
+          </div>
+        )}
+      </div>
+
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourite Movies" />
+      </div>
+      <div className="row">
+        <MovieList
+          movieIdList={favourites}
+          handleFavouritesClick={removeFavouritesMovie}
+          favouriteComponent={RemoveFavourites}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default MovieCard;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,66 +1,72 @@
-import React from 'react'
-import '../assests/style/MovieList.css'
-//import Movies from '../assests/data/Movie'
+import React, { useState, useEffect, useRef } from "react";
+import "../assests/style/MovieList.css";
 
-const MovieList = (props) => {
-    const FavouriteComponent = props.favouriteComponent
+const MovieList = ({
+  movieIdList,
+  searchValue,
+  handleFavouritesClick,
+  favouriteComponent,
+  favourites,
+}) => {
+  const FavouriteComponent = favouriteComponent;
+  const [movies, setMovies] = useState([]);
+  const prevSearchValue = usePrevious(searchValue);
+  const prevMovieIdList = usePrevious(movieIdList);
 
+  const getMovieRequest = async (i) => {
+    const url = `http://www.omdbapi.com/?i=${i}&apikey=b0967f48`;
 
-    const getMovieRequest = async (i) => {
-        const url = `http://www.omdbapi.com/?i=${i}&apikey=b0967f48`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+  };
 
-        const response = await fetch(url); 
-        const responseJson = await response.json();
-       return responseJson
+  function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
+
+  useEffect(async () => {
+    //if (searchValue !== prevSearchValue) {
+    if (
+      (searchValue && searchValue !== prevSearchValue) ||
+      movieIdList?.length !== prevMovieIdList?.length
+    ) {
+      setMovies([]);
+      const movies = await Promise.all(movieIdList.map(getMovieRequest));
+      setMovies(movies);
     }
+  });
 
-   // getMovieRequest()
+  return (
+    <>
+      {movies.map((movie, index) => (
+        <div className="col-md-4 image-container" key={`movie-card-${index}`}>
+          {movie.Poster !== "N/A" && <img src={movie.Poster} alt="Poster" />}
 
-   console.log(props.movieIdList)
+          <h3 className="movie-title">{movie.Title}</h3>
 
-   return <div> {(props.movieIdList || []).map( async (id, index) => {
-    const movie =  await getMovieRequest(id)
-
-    
-    return <div className="col-md-4 image-container" key={`movie-card-${index}`}>
-                <img src={movie.Poster} alt= "Poster" />
-                
-                {/* <h3 className="movie-title">{movie.Title}</h3>
-
-                <button    
-                    onClick={() => props.handleFavouritesClick(movie)}
-                    className='overlay btn btn-dark btn-lg'
-                >
-                    <FavouriteComponent/>  
-                </button>
-                
-                <h5 className="rating">{movie.Ratings}</h5> */}
-            </div>
-
-})} </div>
-
-
-
-    // return (
-    //     <>
-    //         {props.movies.map((movie, index) => (
-    //             <div className="col-md-4 image-container" key={`movie-card-${index}`}>
-    //                 <img src={movie.Poster} alt= "Poster" />
-                    
-    //                 <h3 className="movie-title">{movie.Title}</h3>
-
-    //                 <button    
-    //                     onClick={() => props.handleFavouritesClick(movie)}
-    //                     className='overlay btn btn-dark btn-lg'
-    //                 >
-    //                     <FavouriteComponent/>  
-    //                 </button>
-                    
-    //                 <h5 className="rating">{movie.Ratings}</h5>
-    //             </div>
-    //         ))}
-    //     </>
-    // );
+          {(favourites && !favourites.includes(movie.imdbID)) || !favourites ? (
+            <button
+              // onClick={() => handleFavouritesClick(movie)}
+              onClick={() => handleFavouritesClick(movie.imdbID)}
+              className="overlay btn btn-dark btn-lg"
+            >
+              <FavouriteComponent />
+            </button>
+          ) : (
+            ""
+          )}
+          {movie.imdbRating && <h5 className="rating">{movie.imdbRating}</h5>}
+          {/* {movie.Plot !== "N/A" && <h5 className="year">{movie.Plot}</h5>} */}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default MovieList;
